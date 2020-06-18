16d79fdf5a2313287436dadf8dbaf092cfd2a681
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-06-18 07:13:35 -0700

    moving zeros and product of all numbers are passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,24 +5,30 @@ Returns: an integer
 
 import sys
 
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
     #base cases
     if n == 0:
         return 1
-    if n == 1:
+    elif n == 1:
         return 1
-    if n == 2:
+    elif n == 2:
         return 2
-    if n == 3:
+    elif n == 3:
         return 4
+    elif cache and cache[n]:
+        return cache[n]
+    else:
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    return cache[n]
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        num_cookies = int(sys.argv[1])
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_cookies = int(sys.argv[1])
+#     # Use the main function here to test out your implementation
+#     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+#     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+
+print(eating_cookies(40, {}))
--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,8 +4,13 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
+    my_arr = [0] * len(arr)
+    total = 1
+    for i in range(len(arr)):
+        total = total * arr[i]
+    for i in range(len(arr)):
+        my_arr[i] = int(total/arr[i])
+    return my_arr
 
 
 if __name__ == '__main__':
@@ -14,3 +19,4 @@ if __name__ == '__main__':
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+
